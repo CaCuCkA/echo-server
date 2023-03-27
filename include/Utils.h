b21c61e48a90da0d80bdeb264536f847d59cfa59
Mykola <mykola.yakovkin@ucu.edu.ua>
2023-03-27 10:27:39 +0300

    modified [Utils.h], remove unuse functions

--- include/Utils.h
+++ include/Utils.h
@@ -6,41 +6,20 @@
 #include <unistd.h>
 #include <cstring>
 #include <sys/socket.h>
+#include <sstream>
+#include <map>
 
 namespace
 {
     constexpr uint8_t SOCKET_ERROR = -1;
-    constexpr uint16_t BUFFER_SIZE = 4096;
 }
 
-void Check(uint8_t expression, const std::string& message)
+void Check(uint8_t expression, const std::string &message)
 {
     if (expression == SOCKET_ERROR)
     {
         std::cerr << message << std::endl;
         exit(-1);
     }
-}
-
-
-int HandleConnection(uint16_t clientSocket)
-{
-    char buffer[BUFFER_SIZE];
-    size_t bytesReceive;
-
-    memset(buffer, 0, BUFFER_SIZE);
-
-    bytesReceive = recv(clientSocket, buffer, BUFFER_SIZE, 0);
-    Check(bytesReceive, "There was a connect issue");
-
-    if (bytesReceive == 0)
-    {
-        std::cout << "The client disconnected" << std::endl;
-        return 0;
-    }
-
-    std::cout << "Received: " << std::string(buffer, 0, bytesReceive) << std::endl;
-
-    send(clientSocket, buffer, bytesReceive + 1, 0);
 }
 #endif //UTILS_H
