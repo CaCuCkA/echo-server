50015dd1f995e2be5e08a0905e4bbf612c136307
Mykola <mykola.yakovkin@ucu.edu.ua>
2023-04-21 14:08:31 +0300

    Create [Server.cpp]

--- include/Server.h
+++ include/Server.h
@@ -1,13 +1,18 @@
 #ifndef ECHO_SERVER_SERVER_H
 #define ECHO_SERVER_SERVER_H
 
-#include "Websocket.h"
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "Base.h"
+#include "CrossType.h"
 
 #include <string>
 #include <functional>
 
 
-class Server
+class Server : Base
 {
 public:
     Server() = delete;
@@ -16,13 +21,22 @@ public:
     Server(Server&&) = delete;
     Server& operator=(Server&&) = delete;
 
-    Server(const std::string& t_address, unsigned char t_port, const std::function<void()> t_function);
-    ~Server();
+    Server(std::string  t_address, uint16_t t_port, const std::function<void(cross_types::socket_type&)>& t_function);
+    ~Server() override = default;
 
     void Run();
+
 private:
-    Websocket m_websocket;
-    std::function<void()> m_function;
+    cross_types::address_type MakeAddress() override;
+
+    void Create(cross_types::socket_type& socket) override;
+    void Bind(cross_types::socket_type& socket) override;
+    void Listen(cross_types::socket_type& socket) override;
+
+private:
+    std::string m_address;
+    uint16_t m_port;
+    std::function<void(cross_types::socket_type&)> m_function;
 };
 
 #endif //ECHO_SERVER_SERVER_H
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,12 +1,64 @@
 #include "Server.h"
+#include "CrossType.h"
+#include "Exception.h"
 
-Server::Server(const std::string &t_address, unsigned char t_port, const std::function<void()> t_function)
+#include <utility>
+#include <cstring>
+#include <iostream>
+
+Server::Server(std::string t_address, uint16_t t_port,
+               const std::function<void(cross_types::socket_type&)>& t_function)
     :
-    m_websocket(t_address, t_port), m_function(t_function)
+    m_address(std::move(t_address)), m_port(t_port), m_function(t_function)
 {}
+
+
+cross_types::address_type Server::MakeAddress()
+{
+    cross_types::address_type address{};
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, m_address.c_str(), &address.sin_addr);
+    address.sin_port = htons(m_port);
+    return address;
+}
+
+void Server::Create(cross_types::socket_type& t_socket)
+{
+    t_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (t_socket == EC_CANT_CREATE_SOCKET)
+    {
+        throw EXCEPTION(EC_CANT_CREATE_SOCKET, "Can`t create socket");
+    }
+}
+
+void Server::Bind(cross_types::socket_type &t_socket)
+{
+    cross_types::address_type address = MakeAddress();
+
+    auto error = bind(t_socket,
+                      reinterpret_cast<cross_types::socket_address*>(&address),
+                      sizeof(address));
+    if(error == EC_CANT_BIND_SOCKET)
+    {
+        throw EXCEPTION(EC_CANT_BIND_SOCKET, "Can`t bind socket");
+    }
+}
+
+void Server::Listen(cross_types::socket_type &t_socket)
+{
+    auto error = listen(t_socket, SOMAXCONN);
+    if (error == EC_CANT_LISTEN_SOCKET)
+    {
+        throw EXCEPTION(EC_CANT_LISTEN_SOCKET, "Failed to listen socket");
+    }
+}
 
 void Server::Run()
 {
-    m_websocket.Listen();
-    m_function();
+    cross_types::socket_type serverSocket{};
+    Create(serverSocket);
+    Bind(serverSocket);
+    Listen(serverSocket);
+    m_function(serverSocket);
 }
