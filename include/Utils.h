5b377622334c33bdad2242850a0fd14846e90794
Mykola <mykola.yakovkin@ucu.edu.ua>
2023-05-04 00:52:35 +0300

    solve problem defining functions

--- include/Utils.h
+++ include/Utils.h
@@ -5,23 +5,19 @@
 #include <cstdint>
 #include <string>
 
-#ifdef MULTI
-bool IsEnoughArguments(uint8_t argument)
-{
-    return argument <= 3;
-}
 
-std::tuple<std::string, uint16_t, uint16_t> ParseValues(char *argv[])
-{
-    return std::make_tuple(std::string(argv[1]), atoi(argv[2]), atoi(argv[3]));
-};
-#else
-bool IsEnoughArguments(uint8_t argument)
+inline bool IsEnoughArguments(uint8_t argument)
 {
     return argument <= 2;
 }
 
-std::tuple<std::string, uint16_t> ParseValues(char *argv[])
+#ifdef MULTI
+inline std::tuple<std::string, uint16_t, uint16_t> ParseValues(char *argv[])
+{
+    return std::make_tuple(std::string(argv[1]), atoi(argv[2]), atoi(argv[3]));
+}
+#else
+inline std::tuple<std::string, uint16_t> ParseValues(char *argv[])
 {
     return std::make_tuple(std::string(argv[1]), atoi(argv[2]));
 };
